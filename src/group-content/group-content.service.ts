30cc450e6cfb23895501488c7347e10b742818d4
ignavan391 <ignatenkoivan61@gmail.com>
2021-04-29 15:32:02 +0600

    remove comments

--- src/group-content/group-content.service.ts
+++ src/group-content/group-content.service.ts
@@ -27,32 +27,6 @@ export class GroupContentService {
   async save(userId: User['id']) {
     return this.groupRepository.save({ userId });
   }
-
-  // REVU: этот метод написан не читаемо
-  // Лучше будет сделать так что группа контента создается
-  // автоматически при загрузке видео. Потом при добавлении
-  // контента в плейлист смотреть к какому экрану он привязан
-  // и выбирать из группы самый подходящий
-  //   async getOptimalContent(id: GroupsContent['id']) {
-  //     const group = await this.findOne(id);
-  //     const sortedContents = group.contents.sort(
-  //       (item1, item2) => item1.height - item2.height,
-  //     );
-
-  //     let idx = sortedContents.findIndex(
-  //       (item) => item.height === dto.height && item.width === dto.width,
-  //     );
-
-  //     if (idx === -1) {
-  //       idx = sortedContents.findIndex(
-  //         (item) => dto.height > item.height && dto.width > item.width,
-  //       );
-  //       if (idx === -1) {
-  //         return sortedContents[0];
-  //       }
-  //     }
-  //     return sortedContents[idx];
-  //   }
 }
 
 @Injectable()
